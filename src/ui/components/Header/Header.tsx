89a9efadacf6111784dcc179aff754a62c394ae2
Eugene <evgeniy.boytsov@gmail.com>
2019-02-03 16:49:46 +0300

    add user mock auth icon

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './Header.scss';
 
 export const Header = () => (
-    <header className="header flex flex_justify_spaced">
-        <h1>Books and Grades</h1>
-        <span className="header__time-ago flex flex_align_ended">many years ago...</span>
+    <header className="header flex flex_justify_spaced flex_align_centered">
+        <h2>Если бы я осознал раньше, почему стоит тратить время на чтение, то здесь было бы больше книг</h2>
+        <img className="header__image"
+            width="32"
+            height="32"
+            src="https://lh3.googleusercontent.com/-PcD1dV3zQ3Y/AAAAAAAAAAI/AAAAAAAAAAA/ACevoQMlnvXEghcpxtbA68hb9wKaT0YPqA/s32-c-mo/photo.jpg"
+            alt=""
+        />
     </header>
 );
